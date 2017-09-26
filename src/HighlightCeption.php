94fe83c797c1681c5a98e7c8cea2ba56b7f4f886
Lukas Mestan <lukas.mestan@hyperia.sk>
2017-09-26 14:54:28 +0200

    fix code standards

--- src/HighlightCeption.php
+++ src/HighlightCeption.php
@@ -18,7 +18,7 @@ class HighlightCeption extends Module
      * @var array
      */
     protected $config = [
-		'cssClassName' => 'hyperia-hc',
+        'cssClassName' => 'hyperia-hc',
         'cssStyle' => [
             'background-color' => 'yellow',
             'color' => 'black',
@@ -26,13 +26,13 @@ class HighlightCeption extends Module
         'timeWait' => 1,
         'module' => 'WebDriver'
     ];
-	
-	/**
-	 * Name to use for css style we inject in page
-	 *
-	 * @var string
-	 */
-	 private $cssClassName = "";
+
+    /**
+     * Name to use for css style we inject in page
+     *
+     * @var string
+     */
+    private $cssClassName = "";
 
     /**
      * Time wait
@@ -64,7 +64,7 @@ class HighlightCeption extends Module
         }
 
         $this->webDriverModule = $this->getModule($this->config['module']);
-		$this->cssClassName = $this->config['cssClassName'];
+        $this->cssClassName = $this->config['cssClassName'];
         $this->webDriver = $this->webDriverModule->webDriver;
         $this->timeWait = floatval($this->config['timeWait']);
         $this->test = $test;
@@ -153,13 +153,13 @@ class HighlightCeption extends Module
     private function highlightText($text)
     {
         try {
-			$this->injectCssClassToPage();
+            $this->injectCssClassToPage();
             $this->debug('[Highlight Text] ' . $text);
             $el = $this->webDriver->findElement(WebDriverBy::xpath("//*[text()[contains(., '{$text}')]]"));
             $origHtml = $this->webDriver->executeScript("let origHtml = arguments[0].innerHTML; let hlghtHtml = arguments[0].innerHTML.replace(/({$text})/g, '<span class=\"{$this->cssClassName}\">$1</span>'); arguments[0].innerHTML = hlghtHtml; return origHtml;", [$el]);
-			$this->webDriverModule->wait($this->timeWait);
-			$this->webDriver->executeScript("arguments[0].innerHTML = arguments[1];", [$el, $origHtml]);
-        } catch(Exception $e) {
+            $this->webDriverModule->wait($this->timeWait);
+            $this->webDriver->executeScript("arguments[0].innerHTML = arguments[1];", [$el, $origHtml]);
+        } catch (Exception $e) {
             $this->debug(sprintf("[Highlight Exception] %s \n%s", $e->getMessage(), $e->getTraceAsString()));
         }
     }
@@ -172,20 +172,20 @@ class HighlightCeption extends Module
     private function highlightElement($selector)
     {
         try {
-			$this->injectCssClassToPage();
+            $this->injectCssClassToPage();
             $locator = $this->getSelector($selector);
             if ($locator) {
                 if (Locator::isXPath($locator)) {
-                  $el = $this->webDriver->findElement(WebDriverBy::xpath($locator));
+                    $el = $this->webDriver->findElement(WebDriverBy::xpath($locator));
                 } else {
-                  // assume css
-                  $el = $this->webDriver->findElement(WebDriverBy::cssSelector($locator));
-              }
-              $className = $this->webDriver->executeScript("let className = arguments[0].className; arguments[0].className += ' {$this->cssClassName}'; return className;", [$el]);
-			  $this->webDriverModule->wait($this->timeWait);
-			  $this->webDriver->executeScript("arguments[0].className = arguments[1];", [$el, $className]);
+                    // assume css
+                    $el = $this->webDriver->findElement(WebDriverBy::cssSelector($locator));
+                }
+                $className = $this->webDriver->executeScript("let className = arguments[0].className; arguments[0].className += ' {$this->cssClassName}'; return className;", [$el]);
+                $this->webDriverModule->wait($this->timeWait);
+                $this->webDriver->executeScript("arguments[0].className = arguments[1];", [$el, $className]);
             }
-        } catch(Exception $e) {
+        } catch (Exception $e) {
             $this->debug(sprintf("[Highlight Exception] %s \n%s", $e->getMessage(), $e->getTraceAsString()));
         }
     }
@@ -221,33 +221,36 @@ class HighlightCeption extends Module
 
         return false;
     }
-	
+
     /**
-    * Creates a css style class to inject into page
-    *
-    * @param array $cssStyleArray
-    * @return string CSS Style Class as a string
-    */
-    private function makeCssClassFrom($cssStyleArray) {
-      $cssClass = ".{$this->cssClassName} {";
-      foreach ($cssStyleArray as $key=>$value) {
-        $cssClass .= "{$key}: {$value};";
-      }
-	  $cssClass .= "}";
-      return $cssClass;
+     * Creates a css style class to inject into page
+     *
+     * @param array $cssStyleArray
+     * @return string CSS Style Class as a string
+     */
+    private function makeCssClassFrom($cssStyleArray)
+    {
+        $cssClass = ".{$this->cssClassName} {";
+        foreach ($cssStyleArray as $key => $value) {
+            $cssClass .= "{$key}: {$value};";
+        }
+        $cssClass .= "}";
+        return $cssClass;
     }
 
     /**
-    * Injects a css style into the page to append to elements
-	* that we want to highlight.
-    */
-	private function injectCssClassToPage() {
-		$cssClassAsString = $this->makeCssClassFrom($this->config['cssStyle']);
- 		$js = "let css = \"{$cssClassAsString}\";" .
-			"let elem = document.createElement('style');" .
-			"elem.type = 'text/css';" .
-			"if (elem.styleSheet) { elem.styleSheet.cssText = css; } else { elem.appendChild(document.createTextNode(css));}" .
-			"document.getElementsByTagName('head')[0].appendChild(elem);";
-		$this->webDriver->executeScript($js);
-	}
+     * Injects a css style into the page to append to elements
+     * that we want to highlight.
+     */
+    private function injectCssClassToPage()
+    {
+        $cssClassAsString = $this->makeCssClassFrom($this->config['cssStyle']);
+        $js = "let css = \"{$cssClassAsString}\";" .
+                "let elem = document.createElement('style');" .
+                "elem.type = 'text/css';" .
+                "if (elem.styleSheet) { elem.styleSheet.cssText = css; } else { elem.appendChild(document.createTextNode(css));}" .
+                "document.getElementsByTagName('head')[0].appendChild(elem);";
+        $this->webDriver->executeScript($js);
+    }
+
 }
